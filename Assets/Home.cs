9c0d10324637f78ceae7267dea36ee81c9851986
wwkb123 <wwkb1233@gmail.com>
2019-01-08 22:11:54 -0500

    add Home functions

---
+++ Assets/Home.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Home : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape)) 
+			Application.Quit(); // quit the app on phone
+	}
+
+	public void queueOnClick(){
+		SceneManager.LoadScene("QueueScene");
+	}
+
+	public void stackOnClick(){
+		SceneManager.LoadScene("StackScene");
+	}
+}
